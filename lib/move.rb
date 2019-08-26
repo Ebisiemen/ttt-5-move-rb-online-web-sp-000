f3af82c11a20e410aec90134b67c6f8d095498f9
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-08-26 00:03:13 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,5 +12,8 @@ def input_to_index(user_input)
 end   
   
 def move(array, index, value = "X")
-  array[0] = "X" 
+  def update_array_at_with(array, index, value)
+    array[index] = value 
+    update_array_at_with(board, 0, "X")
+  end   
 end   
