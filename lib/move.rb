3aa35da5c66848b05a26c0b23ed618dcd3379fde
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-08-26 00:03:42 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,6 +14,6 @@ end
 def move(array, index, value = "X")
   def update_array_at_with(array, index, value)
     array[index] = value 
-    update_array_at_with(board, 0, "X")
+    update_array_at_with(array, 0, "X")
   end   
 end   
